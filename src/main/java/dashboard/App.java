86e7ea4d3598cd5a6d01cd6d56f67880cb409471
Sasikanth <shashijagtap9@gmail.com>
2018-09-13 16:48:57 +0530

    add sop

--- src/main/java/dashboard/App.java
+++ src/main/java/dashboard/App.java
@@ -7,7 +7,7 @@ package dashboard;
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException
-    {
+    {System.out.println("perform login");
         SampleTest test=new SampleTest();
         System.out.println("perform login");
         test.login();
