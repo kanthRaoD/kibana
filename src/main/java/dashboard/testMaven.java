f40ac951bbe32399f1db4549d365f23b4c6128f3
Sasikanth <shashijagtap9@gmail.com>
2018-09-14 11:30:33 +0530

    testmaven class

---
+++ src/main/java/dashboard/testMaven.java
@@ -0,0 +1,10 @@
+package dashboard;
+
+public class testMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Executed successfully");
+	}
+
+}
